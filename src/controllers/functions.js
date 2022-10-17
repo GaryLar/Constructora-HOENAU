c9233bd4bb11129a48809324f25e1cd9d14ccf6d
Gary <gary.200114@gmail.com>
2022-10-17 20:06:45 -0300

    Correcion de variables

--- src/controllers/functions.js
+++ src/controllers/functions.js
@@ -1,11 +1,11 @@
 const nodemailer=require("nodemailer")
-
+require('dotenv').config();
 const transporter=nodemailer.createTransport({
         host:"smtp.gmail.com",
         port:587,
         auth:{
             user:"solutions.lardev@gmail.com",
-            pass:"qngkptaniqmsqbrf"
+            pass:process.env.PASSW
         }
 })
 
