51424328ba83cc083b232068e850de19e58e3dc8
Gary <gary.200114@gmail.com>
2022-10-25 13:19:26 -0300

    home responsive

---
+++ public/js/zoom.js
@@ -0,0 +1,14 @@
+/* const zoom=document.querySelectorAll(".proyecto1")
+const imgZ=document.querySelectorAll(".imgZ")
+const prueba=document.querySelectorAll(".zoom")
+
+ zoom.addEventListener("mouseover",(e)=>{
+prueba.classList.add("active2")
+imgZ.classList.add("active2")
+})
+zoom.addEventListener("mouseout",(e)=>{
+    prueba.classList.remove("active2")
+    imgZ.classList.remove("active2")
+    }) 
+
+ */
